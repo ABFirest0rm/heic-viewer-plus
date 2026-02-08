7be90a77e881717e9fc501b15bd3ed5d66af9198
Abey Ajit <ajitabey@yahoo.com>
2026-02-08 22:18:17 +0530

    icon fixes

--- src/heic_viewer/__main__.py
+++ src/heic_viewer/__main__.py
@@ -1,12 +1,27 @@
 import sys
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QTimer
+from pathlib import Path
 
 from heic_viewer.main_window import HeicViewer
 
-
 def main():
+    if sys.platform == "win32":
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "HeicViewerPlus.App"
+        )
+
+    icon_path = None
+
+    if hasattr(sys, "_MEIPASS"):
+        icon_path = Path(sys._MEIPASS) / "assets/icon.png"
+
     app = QApplication(sys.argv)
+
+    if icon_path:
+        app.setWindowIcon(QIcon(str(icon_path)))
 
     window = HeicViewer()
 
@@ -16,7 +31,6 @@ def main():
 
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
