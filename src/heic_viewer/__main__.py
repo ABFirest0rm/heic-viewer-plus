7f1e080bfdb4c952bfb024d075a40b2a4c81f2eb
Abey Ajit <ajitabey@yahoo.com>
2026-02-08 18:09:21 +0530

    open with support

--- src/heic_viewer/__main__.py
+++ src/heic_viewer/__main__.py
@@ -1,4 +1,22 @@
-from heic_viewer.app import run
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTimer
+
+from heic_viewer.main_window import HeicViewer
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = HeicViewer()
+
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        QTimer.singleShot(0, lambda: window.handle_file(file_path))
+
+    window.show()
+    sys.exit(app.exec())
+
 
 if __name__ == "__main__":
-    run()
+    main()
